f4cfa74feb506e509195fa02409afd0b7eaf4186
SVPH\shalom.pel <shalom.pel@softvision.com>
2022-09-26 18:53:14 +0800

    Edited UI for fragment product details, added Size and Add-on

--- app/src/main/java/csv/masters/myapplication/presentation/home/HomeFragment.kt
+++ app/src/main/java/csv/masters/myapplication/presentation/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,6 +20,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var coffeeItems: ArrayList<CoffeeResponseItem>
+    private lateinit var search : SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
