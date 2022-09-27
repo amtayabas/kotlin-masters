c20ec9f3a6b62406e98846f59075b3e97ae87a88
SVPH\michelle.tedria <michelle.tedria@softvision.com>
2022-09-27 16:53:06 +0800

    Added Login UI

--- app/src/main/java/csv/masters/myapplication/presentation/login/LoginActivity.kt
+++ app/src/main/java/csv/masters/myapplication/presentation/login/LoginActivity.kt
@@ -1,10 +1,13 @@
 package csv.masters.myapplication.presentation.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import csv.masters.myapplication.R
 import csv.masters.myapplication.databinding.ActivityLoginBinding
 import csv.masters.myapplication.databinding.LayoutSignupHeaderBinding
+import csv.masters.myapplication.presentation.home.HomeActivity
+import csv.masters.myapplication.presentation.signup.ConfirmCodeActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -24,6 +27,17 @@ class LoginActivity : AppCompatActivity() {
         headerBinding.tvTitle.text = getString(R.string.login)
         headerBinding.ivBack.setOnClickListener {
             onBackPressed()
+        }
+
+        with(binding) {
+            tvPhoneNumber.text = "Phone Number"
+            tvPassword.text = "Password"
+            btnNext.text = "Next"
+        }
+
+        binding.btnNext.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
